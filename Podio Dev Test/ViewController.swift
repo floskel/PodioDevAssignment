9c8cbf8c0ad5aa358c4c5c3209045ac5c4ef114e
Karlo Kristensen <karlo@floskel.com>
2014-11-17 19:10:32 +0100

    Initial Commit

---
+++ Podio Dev Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Podio Dev Test
+//
+//  Created by Karlo Kristensen on 17/11/14.
+//  Copyright (c) 2014 floskel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podio Dev TestTests/Podio_Dev_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  Podio_Dev_TestTests.swift
+//  Podio Dev TestTests
+//
+//  Created by Karlo Kristensen on 17/11/14.
+//  Copyright (c) 2014 floskel. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Podio_Dev_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
